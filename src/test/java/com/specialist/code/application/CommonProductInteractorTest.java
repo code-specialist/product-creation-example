a3b094646601f1765051983ebcdff6ba5ba4aeb5
Matthias Klee <matthias.klee97@gmail.com>
2022-11-16 18:22:02 +0100

    Update CommonProductInteractor tests to cover all possible lines of code

--- src/test/java/com/specialist/code/application/CommonProductInteractorTest.java
+++ src/test/java/com/specialist/code/application/CommonProductInteractorTest.java
@@ -1,6 +1,7 @@
 package com.specialist.code.application;
 
 import com.specialist.code.application.boundaries.output.ICommonProductRegisterGateway;
+import com.specialist.code.application.interactors.CommonProductInteractor;
 import com.specialist.code.application.model.request.CommonProductRequestModel;
 import com.specialist.code.application.model.response.CommonProductResponseModel;
 import com.specialist.code.application.presenter.ICommonProductPresenter;
@@ -23,27 +24,80 @@ public class CommonProductInteractorTest {
     @Mock
     ICommonProductRegisterGateway mockedGateway;
 
-    CommonProductRequestModel requestModel;
-
     @BeforeEach
     void Setup(){
-        requestModel = new CommonProductRequestModel("TestId", "ValidTestName", "Test description", 52.2);
     }
 
     @Test
     void givenValidCommonProductProperties_whenCreate_thenSaveItAndPrepareSuccessView(){
+        // ARRANGE
+        CommonProductRequestModel requestModel = new CommonProductRequestModel("TestId", "ValidTestName", "Test description", 52.2);
 
         long timestmap = 1668617824L;
         CommonProduct product = new CommonProduct("001", "ValidName", "Some Description", 25.25d, timestmap);
-
+        Mockito.when(mockedGateway.existsById(requestModel.getId())).thenReturn(false);
         Mockito.when(mockedFactory.create(requestModel.getId(), requestModel.getName(), requestModel.getDescription(), requestModel.getPrice())).thenReturn(product);
-
-
-        Mockito.verify(mockedGateway, Mockito.times(1)).save(product);
-
         CommonProductResponseModel responseModel = Mockito.verify(mockedPresenter.prepareSuccessView(new CommonProductResponseModel(product.getId(), product.getName(), product.getDescription(), product.getPrice(), String.valueOf(timestmap))), Mockito.times(1));
+        CommonProductInteractor interactor = new CommonProductInteractor(mockedPresenter, mockedFactory, mockedGateway);
 
+        // ACT
+        interactor.create(requestModel);
+
+        // ASSERT
         Assertions.assertEquals("2022-11-16", responseModel.getCreatedAt());
 
+        Mockito.verify(mockedGateway, Mockito.times(1)).save(product);
+        Mockito.verify(mockedGateway, Mockito.times(1)).existsById(requestModel.getId());
     }
+
+    @Test
+    void givenExistingProduct_whenCreate_thenPrepareFailView_withAlreadyContainsMessage(){
+        // ARRANGE
+        CommonProductRequestModel requestModel = new CommonProductRequestModel("TestId", "123", "Test description", 52.2);
+
+        long timestmap = 1668617824L;
+
+
+        CommonProduct product = new CommonProduct("001", "123", "Some Description", 25.25d, timestmap);
+        Mockito.when(mockedGateway.existsById(requestModel.getId())).thenReturn(true);
+        Mockito.when(mockedFactory.create(requestModel.getId(), requestModel.getName(), requestModel.getDescription(), requestModel.getPrice())).thenReturn(product);
+
+        String expectedMessage = "Name 123 is not valid";
+
+        // ACT
+        Exception failViewException = Assertions.assertThrows(Exception.class, () -> {
+            mockedPresenter.prepareFailView(expectedMessage);
+        });
+
+        // ASSERT
+        Assertions.assertTrue(failViewException.getMessage().contains(expectedMessage));
+        Mockito.verify(mockedGateway, Mockito.times(1)).existsById(requestModel.getId());
+    }
+
+
+    @Test
+    void given123Name_whenCreate_thenPrepareFailView_withNameInvalidMessage(){
+
+        // ARRANGE
+        CommonProductRequestModel requestModel = new CommonProductRequestModel("TestId", "123", "Test description", 52.2);
+
+        long timestmap = 1668617824L;
+
+        CommonProduct product = new CommonProduct("001", "123", "Some Description", 25.25d, timestmap);
+
+        Mockito.when(mockedGateway.existsById(requestModel.getId())).thenReturn(false);
+        Mockito.when(mockedFactory.create(requestModel.getId(), requestModel.getName(), requestModel.getDescription(), requestModel.getPrice())).thenReturn(product);
+
+        String expectedMessage = "Name 123 is not valid";
+
+        // ACT
+        Exception failViewException = Assertions.assertThrows(Exception.class, () -> {
+           mockedPresenter.prepareFailView(expectedMessage);
+        });
+
+        // ASSERT
+        Assertions.assertTrue(failViewException.getMessage().contains(expectedMessage));
+        Mockito.verify(mockedGateway, Mockito.times(1)).existsById(requestModel.getId());
+    }
+
 }
